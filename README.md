95b24210c46c16f985796031053986d7e534952d
vlad.lopatynets <v.lopatynets@ukr.net>
2023-08-07 20:55:08 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MVVMHelper
+
+A description of this package.
---
+++ Tests/MVVMHelperTests/MVVMHelperTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import MVVMHelper
+
+final class MVVMHelperTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(MVVMHelper().text, "Hello, World!")
+    }
+}
